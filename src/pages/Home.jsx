d93046ffcf59982e470356ccbc938561c483cbc9
CrystalBlockDev <crytal.blockdev@gmail.com>
2022-07-27 23:56:58 +0900

    making favorit button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -316,7 +316,8 @@ export default function Home() {
         }
       }).then((res) => {
         if (res.data && res.data.code === 0) {
-          getAllFromSmartContract();
+          if(globalWeb3 && chainId && account) getAllFromSmartContract();
+          else getAllFromDB();
         }
       }).catch((err) => {
         console.error(err);
@@ -358,19 +359,19 @@ export default function Home() {
           <div className="flex justify-between items-center mb-6">
             <h2 className='text-xl font-bold dark:text-gray-100'>Explore Grants</h2>
             <div className='hidden md:flex flex-wrap items-start'>
-              <div class="relative  text-gray-600 focus-within:text-gray-400">
-                <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+              <div className="relative  text-gray-600 focus-within:text-gray-400">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                   <button
-                    class="p-1 focus:outline-none focus:shadow-outline"
+                    className="p-1 focus:outline-none focus:shadow-outline"
                   >
                     <svg
                       fill="none"
                       stroke="#3FABB3"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       viewBox="0 0 24 24"
-                      class="w-6 h-6"
+                      className="w-6 h-6"
                     >
                       <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                     </svg>
@@ -379,9 +380,9 @@ export default function Home() {
                 <input
                   type="search"
                   name="q"
-                  class="py-2 text-sm dark:text-white text-gray-900 bg-white dark:bg-gray-900 pl-10 focus:outline-none focus:bg-white focus:dark:text-white focus:text-gray-900 rounded-3xl"
+                  className="py-2 text-sm dark:text-white text-gray-900 bg-white dark:bg-gray-900 pl-10 focus:outline-none focus:bg-white focus:dark:text-white focus:text-gray-900 rounded-3xl"
                   placeholder="Search..."
-                  autocomplete="off"
+                  autoComplete="off"
                   value={searchingName}
                   onChange={(e)=>{setSearchingName(e.target.value)}}
                 />
@@ -411,19 +412,19 @@ export default function Home() {
             </div>
           </div>
           <div className="md:hidden flex lg:flex-wrap md:flex-nowrap items-center my-10">
-              <div class="relative  text-gray-600 focus-within:text-gray-400">
-                <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+              <div className="relative  text-gray-600 focus-within:text-gray-400">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                   <button
-                    class="p-1 focus:outline-none focus:shadow-outline"
+                    className="p-1 focus:outline-none focus:shadow-outline"
                   >
                     <svg
                       fill="none"
                       stroke="#3FABB3"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       viewBox="0 0 24 24"
-                      class="w-6 h-6"
+                      className="w-6 h-6"
                     >
                       <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                     </svg>
@@ -432,9 +433,9 @@ export default function Home() {
                 <input
                   type="search"
                   name="q"
-                  class="py-2 text-sm dark:text-white text-gray-900 bg-white dark:bg-gray-900 pl-10 focus:outline-none focus:bg-white focus:dark:text-white focus:text-gray-900 rounded-3xl"
+                  className="py-2 text-sm dark:text-white text-gray-900 bg-white dark:bg-gray-900 pl-10 focus:outline-none focus:bg-white focus:dark:text-white focus:text-gray-900 rounded-3xl"
                   placeholder="Search..."
-                  autocomplete="off"
+                  autoComplete="off"
                   value={searchingName}
                   onChange={(e)=>{setSearchingName(e.target.value)}}
                 />

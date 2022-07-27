828a8c189464229babe77ce3d68e37e7c440a54f
CrystalBlockDev <crytal.blockdev@gmail.com>
2022-07-28 02:43:24 +0900

    update footer bg

--- src/components/user/UserFooter.jsx
+++ src/components/user/UserFooter.jsx
@@ -9,17 +9,17 @@ const UserFooter = () =>
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
     
     return (
-        <div className="footer flex justify-center  dark:bg-slate-900' items-center" style={{ marginTop:"100px" }}>
-            <NavLink className="handcursor" to="/">
+        <div className="footer flex justify-center  dark:bg-slate-900 items-center" style={{ paddingTop:"100px" }}>
+            <NavLink className="handcursor  dark:bg-slate-900' " to="/">
                 <img width={92} height={76} className="handCursor" src="/images/logo.png" alt="logo" />
             </NavLink>
-            <a className="handcursor" href="ipfs://bafybeigoqsn4s5hnr32olfj7tqrqwz2lejmby4y5to75gnzpvdlm23g76a/#/givestation.eth" target="_blank">
+            <a className="handcursor  dark:bg-slate-900" href="ipfs://bafybeigoqsn4s5hnr32olfj7tqrqwz2lejmby4y5to75gnzpvdlm23g76a/#/givestation.eth" target="_blank">
                 <h1 className="handCursor text-slate-900 dark:text-white font-bold text-xl ml-16">
                 GiveDAO
                 </h1>
             </a>
             {isTabletOrMobile !== true && 
-                <h1 className="handCursor text-slate-900 dark:text-white font-bold text-xl ml-12">
+                <h1 className="handCursor  dark:bg-slate-900' text-slate-900 dark:text-white font-bold text-xl ml-12">
                 About
                 </h1>
             }
